39a4f41cee04a87d5db5ad14c1e75f3d25462cd7
Vasile Filipescu <filipescu.vasile@gmail.com>
2019-07-26 20:12:58 +0200

    Added commentaries and short implementation to open() method in SQLite class

--- TestDBConnection/src/dblib.cpp
+++ TestDBConnection/src/dblib.cpp
@@ -1,22 +1,40 @@
-// dblib.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//                                    .-""-.
+//                                   / _  _ \
+//                                   |(_)(_)|
+// .------------------------------ooO(_ /\ _)Ooo-----------------------------.
+// |                                  |====|                                 |
+// |                                  '-..-'                                 |
+// | Desc:     DBDriver interface                                            |
+// | By:       Nuroferatu - https://github.com/Nuroferatu                    |
+// |                                                                         |
+// | Copyright (C)2018 SoftwareToolsFactory                                  |
+// |                   http://softwaretoolsfactory.com                       |
+// '-------------------------------------------------------------------------'
+// ----= Change log =---------------------------------------------------------
+//    2. 2018.07.26, 20:00 Vasile Filipescu	SQLIteDBDriver::open()--> to test
+//											connection to a sqlite db
+//    1. 2019.07.24, 21:00 Nuroferatu   	[+] Initial
+// ---------------------------------------------------------------------------
 
 #include <iostream>
 #include "idbdriver.h"
 
 using namespace stf;
-
+/*
+ * @brief	Class SQLIteDBDriver
+ * 			Implements IDBDriver for providing an object to db of type SQLite*/
 class SQLIteDBDriver : public IDBDriver {
 public:
+	/*##-Ctor-	*/
     SQLIteDBDriver() {
         std::cout << "SQLIteDBDriver::cTor" << std::endl;
         db = nullptr;
     }
-
+    /*##-Dtor-	*/
     ~SQLIteDBDriver() {
         std::cout << "SQLIteDBDriver::dTor" << std::endl;
     }
-
+    /*##-1-	*/
     virtual bool open( const std::string& connectionStr) override {
 
     	bool dbOpened = false;
@@ -30,48 +48,58 @@ public:
 
     	return dbOpened;
     }
-
+    /*##-2-	*/
     virtual void close( void ) override {
         std::cout << "SQLIteDBDriver::close" << std::endl;
     }
 private :
     sqlite3 * db;
 };
-
+/*
+ * @brief	Class MySQLDBDriver
+ * 			Implements IDBDriver for providing an object to db of type MySQL*/
 class MySQLDBDriver : public IDBDriver {
 
 public:
+	/*##-Ctor-	*/
     MySQLDBDriver() {
         std::cout << "MySQLDBDriver::cTor" << std::endl;
     }
-
+    /*##-Dtor-	*/
     ~MySQLDBDriver() {
         std::cout << "MySQLDBDriver::dTor" << std::endl;
     }
-
+    /*##-1-	*/
     virtual bool open( const std::string& connectionStr) override {
         std::cout << "MySQLDBDriver::open" << std::endl;
         return true;
     }
-
+    /*##-2-	*/
     virtual void close( void ) override {
         std::cout << "MySQLDBDriver::close" << std::endl;
     }
 };
-
+/*
+ * @brief	Enum class DBType
+ * 			Providing types of db connection*/
 enum class DBType {
     MYSQL, SQLITE
 };
-
+/*
+ * @brief	DBDriver
+ * 			Class providing methods to create the different object types of db connection*/
 class DBDriver{
-
 public:
+	/*##-1-		Return an object for accessing the db
+	 *@param	DBType
+	 *@return	IDBDriver	*/
 	IDBDriver* getDriver(DBType);
 };
 
 IDBDriver* DBDriver::getDriver( DBType type) {
 
     IDBDriver* driver = nullptr;
+
     switch (type) {
         case DBType::SQLITE: driver = new SQLIteDBDriver(); break;
         case DBType::MYSQL:  driver = new MySQLDBDriver(); break;
@@ -82,16 +110,16 @@ IDBDriver* DBDriver::getDriver( DBType type) {
 
 int main()
 {
-	const std::string dbName = "testDB.db";
+	const std::string dbName = "testDB.db";	//db to be connected to
 	IDBDriver *dbDriver = nullptr;
 	DBDriver *dbDriverFactory = nullptr;
 
-	dbDriverFactory = new DBDriver();
+	dbDriverFactory = new DBDriver();	//create a new connection object "factory"
 
-	dbDriver = dbDriverFactory->getDriver(DBType::SQLITE);
+	dbDriver = dbDriverFactory->getDriver(DBType::SQLITE);	//return a SQLite db type
 
 	if(dbDriver){
-		dbDriver->open(dbName);
+		dbDriver->open(dbName);  //try to open the connection to SQLite db
 	}
 
 	return 0;
--- TestDBConnection/src/idbdriver.h
+++ TestDBConnection/src/idbdriver.h
@@ -10,6 +10,10 @@
 // | Copyright (C)2018 SoftwareToolsFactory                                  |
 // |                   http://softwaretoolsfactory.com                       |
 // '-------------------------------------------------------------------------'
+// ----= Change log =---------------------------------------------------------
+//    2. 2018.07.26, 20:00 Vasile Filipescu	Adding commentaries
+//    1. 2019.07.24, 21:00 Nuroferatu   	[+] Initial
+// ---------------------------------------------------------------------------
 #pragma once
 #ifndef __STF_IDBDRIVER_H__
 #define __STF_IDBDRIVER_H__
@@ -18,20 +22,24 @@
 #include "sqlite3.h"
 
 namespace stf {
-
-// Interface - must have virtual destructor
-// #1 Must be virtual
-// #2 Must be pure (=0)
-// #3 Destructor must be virtual
+/*
+ * 	@brief	IDBDriver
+ * 			Interface for providing connection to database */
 class IDBDriver {
 public:
+	/*##-Dtor-	*/
     virtual ~IDBDriver() = default;
-
+    /*##-1-		Open connection to db function
+     *@param	const string & --> name for connecting to db
+     *@return	bool	(0--> db opened OK, 1--> db open error	*/
     virtual bool    open( const std::string& connectionStr) = 0;
+    /*##-2-		Close connection to db function
+     *@param	none
+     *@return	none	*/
     virtual void    close( void ) = 0;
 };
 
-} // namespace stf
+}/*namespace stf*/
 
 #endif /* ndef __STF_IDBDRIVER_H__ */
 // vim: ts=4:sw=4:et:nowrap
