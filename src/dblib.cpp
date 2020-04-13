e2820cb1afb3ddaa3bed0ceaf9b952a1474d16f2
Vasile Filipescu <filipescu.vasile@gmail.com>
2020-04-13 21:26:44 +0200

    Added a small Sqlite DB for testing. And some improvements on Sqlite DB code.

--- src/dblib.cpp
+++ src/dblib.cpp
@@ -20,14 +20,15 @@ using namespace stf;
  * @brief   Main
  */
 int main() {
-     const std::string dbName = "testDB.db"; //db to be connected to
+     const std::string dbName = "sqlite_db_users"; //db to be connected to
 
     std::unique_ptr<DBDriver>  dbDriverFactory = std::make_unique<DBDriver>();   //create a new connection object "factory"
     std::unique_ptr<IDBDriver> dbDriver = dbDriverFactory->getDriver( DBDriver::DBType::SQLITE );          //return a SQLite db type
 
     try{
         dbDriver->open( dbName );
-        dbDriver->execute("select * from genres");
+        //dbDriver->execute("select FirstName from userData");
+        dbDriver->execute( "insert into userData (FirstName)\n values (\"Marcin\");" );
         dbDriver->close();
     }catch(std::string& errMsg){
         std::cout << errMsg;
--- src/sqlitedbdriver.cpp
+++ src/sqlitedbdriver.cpp
@@ -25,34 +25,44 @@ SQLiteDBDriver::~SQLiteDBDriver() {
 
 bool SQLiteDBDriver::open( const std::string& connectionStr ) {
     
-    std::string errMsg = "Db could not be opened\n";
+    std::string errMsg = "###_Error_SQLite_Msg: ";
     
-    if (sqlite3_open_v2( connectionStr.c_str(), &db, SQLITE_ACCESS_READWRITE, nullptr ) != SQLITE_OK)
-        throw errMsg;   
+    if ( sqlite3_open_v2( connectionStr.c_str(), &db, SQLITE_ACCESS_READWRITE, nullptr ) != SQLITE_OK ) {
+		std::string errSqlite = sqlite3_errmsg( db );
+		errMsg.append( errSqlite ).append( "\n" );
+		throw errMsg;
+	}
+        
 }
 
 void SQLiteDBDriver::close( void ) {
     std::cout << "SQLIteDBDriver::close" << std::endl;
+
     sqlite3_close(db);
 }
 
 int SQLiteDBDriver::callback( void* db, int argc, char** colValues, char** colNames ){
 
-	for( int i = 0; i < argc; ++i){
+	for( int i = 0; i < argc; ++i ){
 		std::cout << colNames[i] << ": " << colValues[i] ? colValues[i] : "null";
+		std::cout << std::endl;
 	}
 	std::cout << std::endl;
-
 
 	return 0;
 }
 
 void SQLiteDBDriver::execute( const std::string& exec_statement ){
-
 	std::cout << "SQLIteDBDriver::execute " << std::endl;
-	char** errMsg;
-	sqlite3_exec( db, exec_statement.c_str(), callback, nullptr, errMsg );
 
+	std::string errMsg = "###_Error_SQLite_Msg: ";
+	char *errSqlite = nullptr;
+
+	sqlite3_exec( db, exec_statement.c_str(), callback, nullptr, &errSqlite );
+	if ( nullptr != errSqlite) {
+		errMsg.append(errSqlite).append("\n");
+		throw errMsg;
+	}
 }
 // vim: ts=4:sw=4:et:nowrap
 /* EOF */
